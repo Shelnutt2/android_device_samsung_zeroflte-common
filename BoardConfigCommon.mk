7423d71da171a1bef439279286aff1aaa8421756
Seth Shelnutt <shelnutt2@gmail.com>
2015-04-25 19:50:25 -0400

    First commit based on zeroltetmo work

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,99 @@
+BOARD_VENDOR := samsung
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SOC := exynos7420
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := universal7420
+TARGET_NO_BOOTLOADER := true
+
+# Camera
+USE_CAMERA_STUB := true
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# HWComposer
+BOARD_USES_HWCOMPOSER := true
+
+# SurfaceFlinger
+BOARD_USES_SYNC_MODE_FOR_MEDIA := true
+
+# Kernel
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos7420
+BOARD_KERNEL_SEPARATED_DT := true
+
+# NFC
+BOARD_NFC_HAL_SUFFIX := universal7420
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 24164353
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 35651584
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4110417920
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 123341897728
+BOARD_FLASH_BLOCK_SIZE := 2048
+
+# OpenGL
+USE_OPENGL_RENDERER := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Renderscript
+BOARD_OVERRIDE_RS_CPU_VARIANT_32 := cortex-a15
+BOARD_OVERRIDE_RS_CPU_VARIANT_64 := cortex-a57
+
+WITH_DEXPREOPT := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.samsungexynos7420
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := "/sys/devices/13900000.dsim/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_MTP_DEVICE := /dev/usb_mtp_gadget
+TW_DISABLE_TTF := true
+TW_SCREEN_BLANK_ON_BOOT := true
